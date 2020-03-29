a82c5b5b69e737986c47dfd1a453fedb9d688e95
LoczekLoczek <rekowski.krzysztof@gmail.com>
2020-03-29 22:31:00 +0200

    czwarty commit

--- zanurzone/settings.py
+++ zanurzone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z^k!(duaycl8m+2@0_2)!w-91%qu5$$lxfg5fi*@rh+olzie#6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'zanurzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
